2dfe486d27588247dbd04cc5dafa2fac3c9984b5
Crashim03 <alexteloduarte@gmail.com>
2023-06-07 17:19:21 +0100

    Added features to help Debug

--- src/main/java/com/premiumminds/internship/snail/SnailShellPatternTask.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPatternTask.java
@@ -5,31 +5,40 @@ import java.util.concurrent.Callable;
 public class SnailShellPatternTask implements Callable<int[]> {
 
     int[][] _matrix;
-    int     _n;
+    int _n;
 
     public SnailShellPatternTask(int[][] matrix) {
         _matrix = matrix;
         _n = matrix.length;
     }
 
-    @Override
-    public int[] call() {
-        if (_n == 0) return new int[0];
-        if (_n == 1) return _matrix[0];
-        
-        int[] solution = new int[_n * _n];
-        int d = 0;
-
-        if (_n % 2 == 0)  d = _n / 2;
-        else d = _n / 2 + 1; 
-        System.out.println("d: " + d);
-
+    private void fillLinesMatrix(int[][] matrix, int d) {
         for (int i = 0; i < d; i++) {
             for (int j = i; j < _n - i; j++) {
                 System.out.print(_matrix[i][j] + " ");
             }
             System.out.println();
         }
+    }
+
+    @Override
+    public int[] call() {
+        if (_n == 0)
+            return new int[0];
+        if (_n == 1)
+            return _matrix[0];
+
+        int[] solution = new int[_n * _n];
+        int d = 0;
+
+        if (_n % 2 == 0)
+            d = _n / 2;
+        else
+            d = _n / 2 + 1;
+
+        fillLinesMatrix(_matrix, d);
+
+        System.out.println("d: " + d);
 
         return solution;
     }
--- src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
+++ src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.Arrays;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,11 +26,30 @@ public class SnailShellPatternTest {
   public SnailShellPatternTest() {
   };
 
-  public void Test(int[][] matrix, int[] expected) 
-    throws InterruptedException, ExecutionException, TimeoutException {
+  private void PrintMatrix(int[][] matrix) {
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix.length; j++)
+        System.out.print(matrix[i][j] + " ");
+      System.out.println();
+    }
+  }
+
+  public void Test(int[][] matrix, int[] expected)
+      throws InterruptedException, ExecutionException, TimeoutException {
     Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
     int[] result = count.get(10, TimeUnit.SECONDS);
-    assertArrayEquals("Wrong result.", result, expected);
+
+    try {
+      assertArrayEquals(result, expected);
+    } catch (AssertionError e) {
+      System.out.println("Wrong result.");
+      System.out.println("Expected: " + Arrays.toString(expected));
+      System.out.println("Got: " + Arrays.toString(result));
+      System.out.println("Matrix:");
+      PrintMatrix(matrix);
+      System.out.println();
+      throw e;
+    }
   }
 
   @Test
@@ -59,33 +79,34 @@ public class SnailShellPatternTest {
   @Test
   public void EmptyTest()
       throws InterruptedException, ExecutionException, TimeoutException {
-    int[][] matrix = { { } };
-    int[] expected = { };
+    int[][] matrix = { {} };
+    int[] expected = {};
     Test(matrix, expected);
   }
+
   @Test
   public void TenByTenTest()
       throws InterruptedException, ExecutionException, TimeoutException {
     int[][] matrix = new int[10][10];
     int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                       20, 30, 40, 50, 60, 70, 90, 100,
-                       99, 98, 97, 96, 95, 94, 93, 92, 91,
-                       81, 71, 61, 51, 41, 31, 21, 11, 
-                       12, 13, 14, 15, 16, 17, 18, 19,
-                       29, 39, 49, 59, 69, 79, 89, 
-                       88, 87, 86, 85, 84, 83, 82,
-                       72, 62, 52, 42, 32, 22,
-                       23, 24, 25, 26, 27, 28,
-                       38, 48, 58, 68, 78,
-                       77, 76, 75, 74, 73,
-                       63, 53, 43, 33,
-                       34, 35, 36, 37,
-                       47, 57, 67,
-                       66, 65, 64,
-                       54, 44,
-                       45, 46,
-                       56, 55 };
-    
+        20, 30, 40, 50, 60, 70, 90, 100,
+        99, 98, 97, 96, 95, 94, 93, 92, 91,
+        81, 71, 61, 51, 41, 31, 21, 11,
+        12, 13, 14, 15, 16, 17, 18, 19,
+        29, 39, 49, 59, 69, 79, 89,
+        88, 87, 86, 85, 84, 83, 82,
+        72, 62, 52, 42, 32, 22,
+        23, 24, 25, 26, 27, 28,
+        38, 48, 58, 68, 78,
+        77, 76, 75, 74, 73,
+        63, 53, 43, 33,
+        34, 35, 36, 37,
+        47, 57, 67,
+        66, 65, 64,
+        54, 44,
+        45, 46,
+        56, 55 };
+
     for (int i = 0; i < 10; i++) {
       for (int j = 0; j < 10; j++)
         matrix[i][j] = i * 10 + j + 1;
